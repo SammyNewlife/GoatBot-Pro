b33c258425699929e3ad940f90023fd345b79bef
Mohammad Akash <eryxenx@yahoo.com>
2026-05-18 11:22:03 +0600

    Update fork.js

--- scripts/cmds/fork.js
+++ scripts/cmds/fork.js
@@ -30,7 +30,7 @@ exports.onChat = async function({event: z, api: y}){
   if(fork){
     y.sendMessage(
 `🔗𝗚𝗶𝘁𝗛𝘂𝗯 𝗙𝗼𝗿𝗸 𝗟𝗶𝗻𝗸:
-https://github.com/EryXenX/GOAT-MESSENGER.git
+https://github.com/EryXenX/GoatBot-Pro.git
 
 🎬 𝗦𝗲𝘁𝘂𝗽 𝗧𝘂𝘁𝗼𝗿𝗶𝗮𝗹👇🏼
 https://youtu.be/gPf_BFhQz_w?si=T1N6sB2DefeTGq2R`,
